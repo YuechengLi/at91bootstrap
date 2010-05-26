cec8b5c776345bc260c33efa12f167bcf56fe9a0
paul <paul@525dfa4b-21ea-4c62-8250-6929d7a2c1ac>
2010-05-26 08:06:31 +0000

    Add wince special files. Add build script and makefile for build bootstrap by wince toolchain.
    git-svn-id: svn://server/OS/Linux/Boot/Bootstrap30/trunk@3750 525dfa4b-21ea-4c62-8250-6929d7a2c1ac

---
+++ driver/user_hw_init.c
@@ -0,0 +1,9 @@
+void	user_hw_init(void)
+{
+	/* 
+	 * This is a placeholder for the users custom init routine
+	 * Overwrite this file with your own routine to init
+	 * your own H/W features.
+	 */
+}
+
